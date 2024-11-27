23047b39f9beeb832e8ccc73aca00d94e33e0a42
cindyptrri <148054114+cindyptrri@users.noreply.github.com>
2024-11-27 12:08:18 +0700

    Add files via upload

---
+++ Pertemuan9/src/pertemuan9/PernyataanFOR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author CINDY
+ */
+public class PernyataanFOR {
+
+public static void main(String[] args){
+int i;
+for(i=1; i<=10; i++){
+System.out.println("Hasil Fungsi FOR ke - "+i);
+}
+}
+
+}
---
+++ Pertemuan9/src/pertemuan9/Pertemuan9.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author CINDY
+ */
+public class Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+int i;
+for(i=1; i<=10; i++){
+System.out.println("Hasil Fungsi FOR ke - "+i);
+}
+}
+}
+    }
+    
+}
